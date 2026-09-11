43905edf49cee85f1d0a64193f1ae4c0a45ecab4
CDangan1405 <daniel.ondrak.025@pslib.cz>
2026-09-11 11:22:56 +0200

    pridal jsem hromadu kodu co nechapu

--- prvni.cs
+++ prvni.cs
@@ -18,3 +18,46 @@ else
 {
     Console.WriteLine("Konverzace se nezdařila");
 }
+
+
+switch (cislo)
+{
+    case 0: Console.WriteLine("nula"); break;
+    case 10: Console.WriteLine("deset"); break;
+    default: Console.WriteLine("Něco jiného"); break;
+}
+
+for int i = 0; i < 10; i++) // cyklus vypis lichych cisel
+{
+    Console.WriteLine(i);
+}
+
+while (cislo > 0)
+{
+    Console.WriteLine(cislo);
+    cislo = cislo - 1;
+    // opakuje dokud podminka
+}
+
+do
+{
+    Console.WriteLine("Cyklus se provede vždy jednou");
+
+} while (cislo > 0); // podminka se kontroluje po provedeni bloku
+
+string[] ovoce = { "jablko", "hruska", "banan" };
+
+for (int i = 0; i < ovoce.Lenght; i++)
+{
+    Console.WriteLine(ovoce[i]);
+}
+
+foreach (var item in ovoce)
+{
+    Console.WriteLine(item);
+}
+
+for(int j = 0, j < 10; j++)
+{
+    Console.Writeline(j);
+}
