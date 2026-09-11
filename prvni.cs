cbda4806472d4db24b04dbe546efd663c651c67a
CDangan1405 <daniel.ondrak.025@pslib.cz>
2026-09-11 11:10:33 +0200

    Pridal jsem css kod if , else

--- prvni.cs
+++ prvni.cs
@@ -1,1 +1,20 @@
 Console.WriteLine("Hello, World!");
+
+int cislo = 10;
+string vstup = "100";
+
+bool uspech = int.TryParse(text, out cislo);
+
+if (uspech && cislo < 10)
+{
+    Console.WriteLine("Konverzace se zdařila tvoje číslo je" + cislo)
+
+    Console.WritLine("Dvojnásobek čísla je" + cislo * 2);
+} else if (uspech &&cislo >= 10)
+  { 
+    Console.Writeline("Číslo je větší nebo rovno 10")
+  }
+else
+{
+    Console.WriteLine("Konverzace se nezdařila");
+}
